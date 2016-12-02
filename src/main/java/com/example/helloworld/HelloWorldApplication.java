ff02a7ae7e7901280e102d9edf502eb830d48219
Ervin Wu <ecwu@live.unc.edu>
2016-12-02 16:40:47 -0500

    fixed http post requests on localhost

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -76,7 +76,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
         // Enable variable substitution with environment variables
        bootstrap.addBundle(new AssetsBundle("/assets/FrontEndDevelopment", "/", "index.html"));
-        bootstrap.addBundle(new AssetsBundle("/assets/FrontEndDevelopment", "/hourset", "index.html"));
+    //    bootstrap.addBundle(new AssetsBundle("/assets/FrontEndDevelopment", "/hourset", "index.html"));
     //   bootstrap.addBundle(new AssetsBundle("/assets", "/js",  "load-events.js"));
    //  bootstrap.addBundle(new AssetsBundle("/assets", "/js", "calendar-events.js"));
    //   bootstrap.addBundle(new AssetsBundle("/assets", "/css", "style.css"));
