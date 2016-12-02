9fe6dcde3262d8745cc2b19978d82d3e4b4a5fea
Leroy Anderson 3 <leroyanderson3@triad.rr.com>
2016-12-02 11:14:07 -0500

    Calendar.html initial view fix

--- src/main/resources/assets/FrontEndDevelopment/modules/components/ScheduleMaker/ScheduleMaker.js
+++ src/main/resources/assets/FrontEndDevelopment/modules/components/ScheduleMaker/ScheduleMaker.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 var ScheduleMaker = React.createClass({
+    componentWillMount: function(){
+        var calendar = document.getElementById('wrapper');
+        calendar.style.visibility = 'visible';
+        calendar.setAttribute( 'style','visibility: visible');
+        console.log("Mounting: " +calendar.style.visibility);  
+    },
+    componentWillUnmount: function(){
+        var calendar = document.getElementById('wrapper');
+        calendar.style.visibility = 'hidden';
+        calendar.setAttribute( 'style','visibility: hidden');
+        console.log("Unmounting: " +calendar.style.visibility);   
+    },
     handleClick: function(){
         alert("The Schedule is being made")
     },
     render: function(){
-        var calendar = document.getElementById('wrapper');
-        if((typeof calendar.style.visibility === undefined)){
-            calendar.style.visibility = 'hidden';
-            calendar.setAttribute( 'style','visibility: hidden');
-        } else if (calendar.style.visibility === 'visible'){
-            calendar.style.visibility = 'hidden';
-            calendar.setAttribute( 'style','visibility: hidden');
-        } else {
-            calendar.style.visibility = 'visible';
-            calendar.setAttribute( 'style','visibility: visible');
-        }
-        console.log(calendar.style.visibility);
-        
+        console.log("Rendering");
         return(
             <div>
-                <h2>Schedule Maker</h2>
+                <h2>New Schedule Maker</h2>
                 <button id= "button" onClick={this.handleClick}>Make Schedule</button>
             </div>
         );
