2310e4cec1f7ade16bfde70767971c6746701988
Leroy Anderson 3 <leroyanderson3@triad.rr.com>
2016-12-07 03:32:07 -0500

    Major Style Changes

--- target/classes/assets/FrontEndDevelopment/modules/App.js
+++ target/classes/assets/FrontEndDevelopment/modules/App.js
@@ -4,14 +4,33 @@ import NavLink from './NavLink'
 export default React.createClass({
   render() {
     return (
-      <div>
-        <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Log In</NavLink></li>
-          <li><NavLink to="/hourset">HourSet</NavLink></li>
-          <li><NavLink to="/weekset">WeekSet</NavLink></li>
-          <li><NavLink to="/calendar">Calendar</NavLink></li>
-        </ul>
-        {this.props.children}
+      <div className="container" style={{
+        height: 100,
+        width: 800
+        }}>
+        <nav className="navbar navbar-inverse -bg-faded">
+            <ul role="nav" className="nav navbar-nav">
+                <li className="nav-item active" style={{
+                    color: 'white'
+                }}>
+                    <h1>Hour Entry Form</h1>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/" onlyActiveOnIndex>Log In</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/hourset">HourSet</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/weekset">WeekSet</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/calendar">Calendar</NavLink>
+                </li>
+            </ul>
+                    {this.props.children}
+        </nav>
+        
       </div>
     )
   }
